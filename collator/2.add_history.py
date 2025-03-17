c147ac0a83d7e576477716ecd3dcd222e771d70a
TrongNV2003 <trongnguyen220603@gmail.com>
2025-03-17 17:03:55 +0700

    refactor: preprocessing

--- collator/2.add_history.py
+++ collator/2.add_history.py
@@ -2,8 +2,7 @@ import json
 from collections import defaultdict
 
 
-def create_history(input_path: str) -> None:
-    output_path = "raw_data/output.json"
+def create_history(input_path: str, output_path: str) -> None:
 
     with open(input_path, "r", encoding="utf-8") as file:
         data = json.load(file)
@@ -18,7 +17,7 @@ def create_history(input_path: str) -> None:
         for idx, message in enumerate(messages):
             history = [msg["text"] for msg in messages[max(0, idx-999): idx]]
             current_message = message["text"]
-            labeled_intent = message["labeled_intent"]
+            labeled_intent = message["label_intent"]
 
             if len(history) == 0:
                 history = []
@@ -38,5 +37,7 @@ def create_history(input_path: str) -> None:
     print(f"Dữ liệu đã được xử lý và lưu tại: {output_path}")
 
 if __name__ == "__main__":
-    input_file = "raw_data/output_dedup.json"
-    create_history(input_file)
+    input_path = "gen_dataset.json"
+    output_path = "output.json"
+
+    create_history(input_path, output_path)
